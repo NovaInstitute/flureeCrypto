aa5c01a8f57f5156c74808e913706d8da4cc30fe
christiaanpauw <christiaan.pauw@nova.org.za>
2024-10-02 20:38:44 +0200

    CJP check ripemd

--- R/ripemd.R
+++ R/ripemd.R
@@ -30,20 +30,22 @@ ripemd_160 <- function(ba) {
 #' @return A character string representing the RIPEMD-160 hash in the specified format.
 #'
 #' @examples
-#' ripemd_160("Hello, world!")
-#' ripemd_160(charToRaw("Hello, world!"), output_format = "base64")
-#'
+#' ripemd_160("hi there!", output_format = "hex")
+#' ripemd_160(charToRaw("hi there!"), output_format = "hex")
+#' # (= "ad6ce46f7f1ea8519dc02ce8ce0c278c6ff329b2" (alphabase/bytes->hex (ripemd-160 (.getBytes "hi there!")))))
+#' ripemd_160(charToRaw("hi there!"), output_format = "base64")
+#' ripemd_160(charToRaw("hi there!"), output_format = "raw")
 #' @import openssl
 #' @import sodium
 #' @export
-ripemd_160 <- function(x, output_format = "hex", input_format = NULL) {
+ripemd_160 <- function(x, output_format = c("hex", "base64", "raw")[3], input_format = NULL) {
 
   # Determine the input format if not provided
   if (is.null(input_format)) {
     input_format <- coerce_input_format(x)
   }
 
-  # Convert input to a raw vector if it is a string
+  # Convert input to a raw vector if it's a string
   if (input_format == "string") {
     x <- charToRaw(normalize_string(x))
   }
@@ -51,12 +53,15 @@ ripemd_160 <- function(x, output_format = "hex", input_format = NULL) {
   # Calculate the RIPEMD-160 hash
   hash_raw <- openssl::ripemd160(x)
 
-  # Convert the hash to the desired output format
+  # Return in the desired format
   if (output_format == "hex") {
     return(bin2hex(hash_raw))
   } else if (output_format == "base64") {
     return(base64enc::base64encode(hash_raw))
+  } else if (output_format == "raw") {
+    return(hash_raw)  # return raw byte array (similar to the Clojure byte array)
   } else {
-    stop("Unsupported output format. Use 'hex' or 'base64'.")
+    stop("Unsupported output format. Use 'hex', 'base64', or 'raw'.")
   }
 }
+
