2cca859cbd03132415fd5abe112949da0f1a473a
christiaanpauw <christiaan.pauw@nova.org.za>
2024-10-02 18:13:37 +0200

    Gefix CJP

--- R/util.R
+++ R/util.R
@@ -8,26 +8,37 @@
 #' @return A raw vector containing the first `n` bytes of the SHA3-512 hash.
 #'
 #' @examples
-#' hash_string_key("example-key", 32)
+#' hash_string_key("hello", 32)
 #' hash_string_key(charToRaw("example-key"), 16)
-#'
-#' @import digest sodium
+#' # fluree.crypto.util=> (hash-string-key "hello" 32)
+#' # (117 -43 39 -61 104 -14 -17 -24 72 -20 -10 -80 115 -93 103 103 -128 8 5 -23 -18 -14 -79 -123 125 95 -104 79 3 110 -74 -33)
+#' # fluree.crypto.util=> (hash-string-key "example-key" 16)
+#' # (-1 115 100 -51 32 13 -97 54 15 -13 -70 41 -90 -10 93 4)
+#' @import digest sodium openssl
 #' @export
 
+# Function to compute the SHA3-512 hash and return the first n bytes
 hash_string_key <- function(key, n = 32) {
   # Ensure n is less than or equal to 64 (since SHA3-512 produces 64 bytes)
   stopifnot(n <= 64)
-  
+
   # Convert the key to raw bytes if it's a string
   if (is.character(key)) {
     key <- charToRaw(key)
   }
-  
-  # Compute the SHA3-512 hash and convert to binary array
-  hash_512 <- sha3_512(key)
-  hash_512 <- hex2bin(hash_512)
-  
-  # Return the first n bytes of the hash
-  return(hash_512[1:n])
+
+  # Compute the SHA3-512 hash
+  hash_512 <- openssl::sha3(key, size = 512)
+
+  # Convert the raw bytes to integers
+  raw_bytes <- as.integer(hash_512)
+
+  # Convert unsigned integers to signed integers for the first n bytes
+  signed_bytes <- ifelse(raw_bytes > 127, raw_bytes - 256, raw_bytes)
+
+  # Return the first n signed bytes of the hash
+  return(signed_bytes[1:n])
 }
+
+
 
