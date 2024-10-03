7aab864fafc60e9f86df1326574f2cdada3a6bd2
christiaanpauw <christiaan.pauw@nova.org.za>
2024-10-03 08:54:51 +0200

    Check Hmac against Clojure and give output options

--- R/hmac.R
+++ R/hmac.R
@@ -5,22 +5,42 @@
 #' @return A raw vector containing the HMAC-SHA256 result.
 #' @import digest
 #' @export
-hmac_sha256 <- function(message, key) {
+#' @examples
+#' # => (require '[alphabase.core :as alphabase])
+#' # => (def message (.getBytes "hello"))
+#' # => (def key (.getBytes "secret"))
+#' # => (println (alphabase/bytes->hex (hmac-sha256 message key)))
+#' # 88aab3ede8d3adf94d26ab90d3bafd4a2083070c3bcce9c014ee04a443847c0b
+#' hmac_sha256(message = charToRaw("hello"), key = charToRaw("secret"))
+#' hmac_sha256(message = charToRaw("hello"), key = charToRaw("secret"), output_format = "hex")
+#' # "88aab3ede8d3adf94d26ab90d3bafd4a2083070c3bcce9c014ee04a443847c0b"
+
+hmac_sha256 <- function(message, key, output_format = c("hex", "base64", "raw")[3]) {
   # Ensure both inputs are raw vectors
   if (!is.raw(message) || !is.raw(key)) {
     stop("Both message and key should be raw vectors.")
   }
-  
+
   # Convert raw vectors to character strings for hashing
   message_str <- rawToChar(message)
   key_str <- rawToChar(key)
-  
+
   # Compute HMAC using SHA256
-  hmac_result <- digest::hmac(key = key_str, 
-                              object = message_str, 
-                              algo = "sha256", 
-                              serialize = FALSE)
-  
+  hmac_result <- digest::hmac(key = key_str,
+                              object = message_str,
+                              algo = "sha256",
+                              serialize = FALSE,
+                              raw = TRUE)
+
   # Convert the result back to raw
-  return(charToRaw(hmac_result))
+  if (output_format == "hex") {
+    return(bin2hex(hmac_result))
+  } else if (output_format == "base64") {
+    return(base64enc::base64encode(hmac_result))
+  } else if (output_format == "raw") {
+    return(hmac_result)
+  } else {
+    stop("Unsupported output format. Use 'hex' or 'base64'.")
+  }
+
 }
